2d89244d0db4c2687d82b3532c2a911c3aed5cba
AA <AA@DESKTOP-F0AQQHS>
2024-12-03 19:23:43 +0500

    first commit

--- lib/widgets/maintab.dart
+++ lib/widgets/maintab.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, sort_child_properties_last, avoid_print
 
 import 'package:flutter/material.dart';
 
@@ -24,7 +24,7 @@ class _MainTabState extends State<Maintab> {
             onPressed: () {
               print("Search Button Clicked");
             },
-            icon: Icon(Icons.search),
+            icon: const Icon(Icons.search),
             splashColor: Colors.transparent,
             highlightColor: Colors.transparent,
           ),
@@ -32,12 +32,12 @@ class _MainTabState extends State<Maintab> {
               BoxDecoration(shape: BoxShape.circle, color: Colors.pink[300]),
         ),
          Container(
-          margin: EdgeInsets.symmetric(horizontal: 10.0),
+          margin: const EdgeInsets.symmetric(horizontal: 10.0),
           child: IconButton(
             onPressed: () {
               print("Menu Button Clicked");
             },
-            icon: Icon(Icons.menu),
+            icon: const Icon(Icons.menu),
             splashColor: Colors.transparent,
             highlightColor: Colors.transparent,
           ),
