366710ccf82e321f8218d998383f0bb2c6e3f98c
AA <muhammadtalalshakeel2009@gmail.com>
2024-12-13 20:21:37 +0500

    first commit

---
+++ lib/screens/pagedetail.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class PageDetail extends StatelessWidget {
+  const PageDetail({super.key, required data});
+
+  @override
+  Widget build(BuildContext context) {
+    var data;
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text(data['title']['rendred']),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+          Center(
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 10, top: 10),
+              child: Text(
+                data['title']['rendered'],
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              data['title']['rendered']
+                  .toString()
+                  .replaceAll("<p>", "")
+                  .replaceAll("/p", ""),
+              style: TextStyle(fontSize: 20.0),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/mydrawer.dart
+++ lib/widgets/mydrawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_api_wordpress/screens/pagedetail.dart';
 import 'package:flutter_api_wordpress/services/page.dart';
 
 class MyDrawer extends StatefulWidget {
@@ -19,34 +20,45 @@ class _MyDrawerState extends State<MyDrawer> {
           children: [
             Expanded(
                 child: Container(
-
-
-              child: 
-              
-              
-        future: postService.getAllPost(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data?.length == 0) {
-              return const Center(
-                child: Text("0 Post"),
-
-              );
-            }
-            return 
-            } else if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-
-            )
+              child: FutureBuilder<List>(
+                future: pageService.getAllPost(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data?.length == 0) {
+                      return const Center(
+                        child: Text("No page"),
+                      );
+                    }
+                    return ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, i) {
+                      return ListTile(
+                        title:  Center(
+                          child: Text(
+                            snapshot.data![i]["title"]["rendered"],
+                            style: TextStyle(fontSize: 22, color: Colors.white),
+                          ),
+                        ),
+                        onTap: () => {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>  Text(PageDetail(data: snapshot.data?[i],) as String)))
+                        },
+                      );
+                    });
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text(snapshot.error.toString()),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ))
           ],
         ),
       ),
