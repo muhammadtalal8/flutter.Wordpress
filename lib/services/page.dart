80882c7c98bf3296fff3e98e01484a83ee8d126e
AA <muhammadtalalshakeel2009@gmail.com>
2024-12-13 19:51:33 +0500

    first commit

---
+++ lib/services/page.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: non_constant_identifier_names, avoid_print
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MyPage {
+  String baseUrl = "https://elearning.com/wp-json/wp/v2/pages";
+
+  getAllPost() {}}
+
+Future<List> getAllPage() async {
+  try {
+    var response = await http.get(Uri.parse(base64Url as String));
+    print(response);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      return Future.error("Server Error");
+    }
+  } catch (SocketException) {
+    return Future.error("Error Fetching Data");
+  }
+}
--- lib/widgets/mydrawer.dart
+++ lib/widgets/mydrawer.dart
@@ -1,16 +1,15 @@
-
-// ignore_for_file: library_private_types_in_public_api, avoid_unnecessary_containers
-
 import 'package:flutter/material.dart';
+import 'package:flutter_api_wordpress/services/page.dart';
 
 class MyDrawer extends StatefulWidget {
-  const MyDrawer(MyDrawer context,  {Key? key}) : super(key: key);
+  const MyDrawer(MyDrawer context, {Key? key}) : super(key: key);
 
   @override
   _MyDrawerState createState() => _MyDrawerState();
 }
 
 class _MyDrawerState extends State<MyDrawer> {
+  MyPage pageService = MyPage();
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -20,21 +19,34 @@ class _MyDrawerState extends State<MyDrawer> {
           children: [
             Expanded(
                 child: Container(
-              child: ListView.builder(
-                itemCount: 3,
-                itemBuilder: (context, i) {
-                  return ListTile(
-                    title: const Center(child: Text("Service",style: TextStyle(fontSize: 22,color: Colors.white),)),
-                    onTap: () => {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Text("Service")))
-                    },
-                  );
-                },
-              ),
-            ))
+
+
+              child: 
+              
+              
+        future: postService.getAllPost(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data?.length == 0) {
+              return const Center(
+                child: Text("0 Post"),
+
+              );
+            }
+            return 
+            } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+
+            )
           ],
         ),
       ),
